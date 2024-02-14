7af79d867ae14f8e4b74bc473b84a072c749362b
Appolinairee <adandappolinaire229@gmail.com>
2024-02-14 17:59:46 +0100

    feat: chat message form

--- src/BaseComponents/ChatUnit.jsx
+++ src/BaseComponents/ChatUnit.jsx
@@ -1,13 +1,28 @@
+import { BiCheck } from "react-icons/bi";
+import { BiCheckShield } from "react-icons/bi";
+
 const ChatUnit = ({message}) => {
   return (
-    <div className="bg-green-700 flex items-end justify-end w-full">
-        <div className={`py-2 max-w-[80%] px-1 rounded-[10px] ${message.type == 1 ? 'bg-primary text-light' : 'bg-light'}`}>
+    <div className="">
+        <div className={`p-2 relative w-fit max-w-[80%] mt-2 rounded-[15px] ${message.type == 1 ? 'bg-primary !text-light left-[20%]' : 'bg-light'}`}>
             <p className="text-[13px]">{message.message}</p>
 
-            <p className="text-[9px] text-dark/70 ">{message.time}</p>
+            <p className={`text-[10px] flex items-center justify-end gap-1 w-[18%] flex-nowrap text-dark/80 ${message.type == 1 ? 'text-light/80 relative left-[78%]' : ''}`}>
+              
+              <span className="whitespace-nowrap">{message.time}</span>
+
+              {
+                message.state && <span>
+                { (message.state == 0) && <BiCheck /> }
+                { (message.state == 1) && <BiCheckShield /> }
+                { (message.state == 2) && <BiCheckShield className="text-blue-700" /> }
+              </span>
+              }
+              
+            </p>
         </div>
     </div>
   )
 }
 
-export default ChatUnit
+export default ChatUnit;
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,11 @@ import { BiArrowFromRight } from "react-icons/bi";
 import Profil from "../assets/photos(exemples)/OIP (3).jpg";
 import { BiFace } from "react-icons/bi";
 import ChatUnit from "../BaseComponents/ChatUnit";
+import { BiPhone } from "react-icons/bi";
+import { BiSticker } from "react-icons/bi";
+import { BiImage } from "react-icons/bi";
+import { BiSolidFileDoc } from "react-icons/bi";
+import { BiFileFind } from "react-icons/bi";
 
 const Chat = () => {
   
@@ -14,7 +19,8 @@ const Chat = () => {
     {
       "message": "Hello! C'est Appolinaire. J'aime bien tes produits. Je peux avoir un canapé à moins de 30.000 Fcfa?",
       "type": 1,
-      "time": "10:10 PM"
+      "time": "10:10 PM",
+      "state": 1
     },
 
     {
@@ -25,14 +31,38 @@ const Chat = () => {
     {
       "message": "Je veux demander un rapport de livraison",
       "type": 1,
+      "time": "10:10 PM",
+      "state": 1
+    },
+    {
+      "message": "Hello! C'est Appolinaire. J'aime bien tes produits. Je peux avoir un canapé à moins de 30.000 Fcfa?",
+      "type": 0,
       "time": "10:10 PM"
-    }
+    },
+    {
+      "message": "Hello! C'est Appolinaire. J'aime bien tes produits. Je peux avoir un canapé à moins de 30.000 Fcfa?",
+      "type": 1,
+      "time": "10:10 PM",
+      "state": 1
+    },
+
+    {
+      "message": "Bonjour Tonton",
+      "type": 0,
+      "time": "10:10 PM"
+    },
+    {
+      "message": "Je veux demander un rapport de livraison",
+      "type": 1,
+      "time": "10:10 PM",
+      "state": 1
+    },
   ]
 
   return (
-    <div className="absolute bg-light top-0 left-0 w-full h-full">
+    <div className="absolute bg-light top-0 left-0 w-full h-full custom-background">
 
-       <div className="absolute pt-2 pb-1 px-2 text-light w-full top-0 left-0 bg-primary  flex items-center h-[9%]">
+       <div className="absolute z-10 pt-2 pb-1 px-2 text-light w-full top-0 left-0 bg-primary flex items-center h-[9%]">
 
           <div className="flex items-center gap-2 text-[13px] ">
             <div className="flex items-center rounded-2xl hover:bg-light/15 gap-[3px] text-[13px] cursor-pointer p-[3px]">
@@ -46,15 +76,34 @@ const Chat = () => {
           <BiFace />
        </div>
 
-       <div className="absolute p-2 flex flex-col gap-1 bottom-0 w-full h-[91%] left-0 bg-red-700 mt-8">
+       <div className="absolute p-2 bottom-0 w-full h-[82%] mb-14 mt-18 left-0 overflow-auto scrollbar-thin bg-transparent">
           {
             Messages.map((message, index ) => (
               <div key={index+message.message}>
                 <ChatUnit message={message} />
-                
+
               </div>
             ))
           }
+        </div>
+        
+        <div className="absolute w-[98%] left-[1%] h-[50px] bg-transparent bottom-1 z-10">
+          <form action="" method="post" className="flex flex-nowrap relative gap-1">
+
+            <div className="shadow-boxShadow1 w-[90%] flex relative items-center rounded-[20px] bg-light">
+               <BiSticker className="absolute  left-[5px] text-dark/90 p-1 hover:bg-dark/20 text-[26px] cursor-pointer rounded-full" />
+
+               <BiImage className="absolute right-[5px] text-dark/90 p-1 bg-dark/10 text-[26px] cursor-pointer rounded-full" />
+
+               <BiSolidFileDoc className="absolute right-[30px] text-dark/90 p-1 hover:bg-dark/10 text-[26px] cursor-pointer rounded-full" />
+
+               <input type="text" name="message" placeholder="Message" className="rounded-[20px] w-full bg-dark/10 pl-8 py-2 text-dark text-[14px]" autoFocus/>
+            </div>
+            
+            <div className="rounded-full w-[10%] bg-primary flex items-center h-[35px] text-light ">
+              <BiPhone className="m-auto text-xl" />
+            </div>
+          </form>
         </div>
 
     </div>
