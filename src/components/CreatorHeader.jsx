edada1e86f7e3b084478153edbe36d4ded66a5b6
Appolinairee <adandappolinaire229@gmail.com>
2024-02-27 22:52:03 +0100

    feat: creator exterior page

--- src/components/CreatorHeader.jsx
+++ src/components/CreatorHeader.jsx
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import Background from "../assets/photos(exemples)/OIP (4).jpeg";
+import Button1 from "../BaseComponents/Button1";
+import Button2 from '../BaseComponents/Button2';
+import Button3 from '../BaseComponents/Button3';
 
 const Header = () => {
 
@@ -39,12 +42,12 @@ const Header = () => {
     ];
 
   return (
-    <div className="min-h-[350px] mt-3 rounded-2xl h-[65vh] relative shadow-boxShadow1 text-light">
-        <div className="relative min-h-full h-full p-[5%] rounded-2xl w-full" style={
-            { background: `url(${ Slides[0].background }) center no-repeat`, backgroundSize: "cover" }
-        }>
-           <div className='z-10 flex !items-start h-full justify-start flex-col'>
-                <div className='flex items-center justify-cente whitespace-nowrapr gap-2 max-w-[200px] text-[14px] font-medium'>
+    <div className="min-h-[350px] mt-3 rounded-2xl h-[65vh] relative shadow-boxShadow1 text-light" style={
+        { background: `url(${ Slides[0].background }) center no-repeat`, backgroundSize: "cover" }
+    }>
+        <div className="relative min-h-full h-full p-[5%] z-30 rounded-2xl w-full">
+           <div className='z-30 flex !items-start h-full justify-start flex-col'>
+                <div className='flex items-center justify-cente whitespace-nowrap gap-2 max-w-[200px] text-[14px] font-medium'>
                     <p className='bg-dark/20 p-1 rounded-lg'> Meubles</p>
                     <p className='bg-dark/20 p-1 rounded-lg'>Tabouret</p>
                 </div>
@@ -52,23 +55,22 @@ const Header = () => {
                 <h3>King Of Soto</h3>
 
                 <span className='text-3xl whitespace-nowrap font-medium'>
-                    Nom du produit
+                    Nom du produit 
                 </span>
                 
-                <p className=' text-[16 px] max-w-[600px]'>
+                <p className=' text-[16 px] max-w-[600px] xs:text-[14px]'>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ut, dicta recusandae tenetur, neque qui magnam rerum reiciendis veritatis inventore aut atque architecto. santium?
                 </p>
 
 
-
                 <div className='flex items-center justify-center gap-[10%] whitespace-nowrap'>
-                    <div>Button 1</div>
-                    <div>Button 2</div>
+                    <Button2 Text="Découvrir" />    
+                    <Button3 />
                 </div>
            </div>
-
-            <div className='absolute z-0 top-0 left-0 h-full w-full bg-dark/20 rounded-2xl '></div>
         </div>
+
+        <div className='absolute z-10 top-0 left-0 h-full w-full bg-dark/70 rounded-2xl '></div>
     </div>
   )
 }
--- src/pages/CreatorProfil.jsx
+++ src/pages/CreatorProfil.jsx
@@ -1,51 +1,38 @@
 import Logo from "../assets/photos(exemples)/OIP (2).jpeg";
 import { BiMessageRoundedDetail } from "react-icons/bi";
 import CreatorHeader from "../components/CreatorHeader";
+import Products from "../components/Products/Products";
 
 const CreatorProfil = () => {
+   return (
+      <div className="pt-10">
+         <div className="mt-12 mx-[3%] bg-light px-3">
+            <div className="flex items-center justify-center py-2 cursor-pointer">
+               <div className=" rounded-tl-full flex items-center gap-2">
+                  <img
+                     src={Logo}
+                     className="rounded-[20px] rounded-tl-[0px] w-[70px] h-[50px]"
+                     alt="Logo de..."
+                  />
+                  <h4 className="text-[19px] font-medium">King of Soto</h4>
+               </div>
 
-  return (
-    <div className="pt-10">
-        <div className="mt-12 mx-[3%] bg-light px-3">
-            <div className="flex items-center justify-center py-2">
-                <div className="rounded-lg flex itesm-center gap-2">
-                    <img src={Logo} className="rounded-2xl w-[70px] h-[50    px]" alt="Logo de..." />
-                    <h4 className="text-[19px] font-medium">King of Soto</h4>
-                </div>
-
-                <div className="flex gap-2 items-center justify-center">
-                    <BiMessageRoundedDetail />
-                    <p>Discussion</p>
-                </div>
+               <div className="flex gap-2 items-center justify-center cursor-pointer">
+                  <BiMessageRoundedDetail />
+                  <p>Discussion</p>
+               </div>
             </div>
-
-            {/* <div className="min-h-[400px] mt-3 rounded-2xl border-solid border-[1px]">
-                <div className="">
-                    <div>
-                        Categories
-                    </div>
-
-                    <div>Nom Produit</div>
-                    Images en background
-                    Petite description du produit
-
-                </div>
-            </div> */}
 
             <CreatorHeader />
 
             <div>
-                Description, Localisation,..
-                Informations (seul pour le créateur)
+               Description, Localisation,.. Informations (seul pour le créateur)
             </div>
 
-            <div>
-                Products
-            </div>
-        </div>
-
-    </div>
-  )
-}
+            <Products />
+         </div>
+      </div>
+   );
+};
 
 export default CreatorProfil;
